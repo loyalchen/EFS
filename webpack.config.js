d933bb3d3baf834a1a2f2fc549476f462850bdc2
LoyalChen <806030391@qq.com>
2015-12-15 09:41:37 +0800

    Update

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,9 @@
 module.exports = {
 	entry: {
-<<<<<<< HEAD
 		tutorial:"./dev/component/tutorial.js"
-=======
 		siCargo:"./dev/flux/siCargo/index.js",
 		test:"./dev/flux/test/index.js",
 		uTest:"./dev/test/siCargoTableTest.js"
->>>>>>> d2ba88d3e3084d97ac5819fc1264d64a6a9894e9
 	},
 	output: {
 		path: __dirname + "/public/built/js/",
