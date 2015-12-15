39bd835643ce9b93cb25fc103ede54853e2a03bc
TedForV <tedforv@outlook.com>
2015-12-15 14:49:31 +0800

    change the boostrap to non-responsive

--- dev/component/requestLayout.js
+++ dev/component/requestLayout.js
@@ -2,6 +2,7 @@ import React from 'react';
 import gStyle from '../globalStyle';
 import SiCargoTable from './siCargoTable';
 import MultiSelectGroup from './multiSelectionGroup';
+import {Router,Route,Link} from 'react-router';
 
 
 class RequestLayout extends React.Component {
@@ -40,17 +41,19 @@ class RequestLayout extends React.Component {
 			var sideBar, table, detail;
 			var {tableWidth} = this.state;
 			var {filterOptions,requestData,currentFilter,onFilterChange} = this.props;
+			var sideBarClassName,tableClassName,detailClassName;
+			
 
 			switch (this.state.module) {
 				case gStyle.constant.PAGE_LIST:
-					sideBar = ( < div className = "col-md-2 col-md-offset-1"><MultiSelectGroup filterOptions={filterOptions} currentFilter={currentFilter}  onFilterChange={onFilterChange} />< /div>);
-					table = ( < div className = "col-md-8" id='fullTable'><SiCargoTable data={requestData} cascadeWidth={tableWidth} handleCheckValueChange={this._handleCheckValueChange} identityColumnName={this.props.identityColumnName} />< /div>);
+					sideBar = ( < div className = "col-xs-2 col-xs-offset-1"><MultiSelectGroup filterOptions={filterOptions} currentFilter={currentFilter}  onFilterChange={onFilterChange} />< /div>);
+					table = ( < div className = "col-xs-8" id='fullTable'><SiCargoTable data={requestData} cascadeWidth={tableWidth} handleCheckValueChange={this._handleCheckValueChange} identityColumnName={this.props.identityColumnName} />< /div>);
 					detail = null;
 					break;
 				case gStyle.constant.PAGE_DETAIL:
 					sideBar = null
-					table = ( < div className = "col-md-2 col-md-offset-1" id='briefTable'><SiCargoTable data={requestData} identityColumnName={this.props.identityColumnName} />< /div>);
-					detail = ( < div className = "col-md-8"> I am detail < /div>);
+					table = ( < div className = "col-xs-2 col-xs-offset-1" id='briefTable'><SiCargoTable data={requestData} identityColumnName={this.props.identityColumnName} />< /div>);
+					detail = ( < div className = "col-xs-8"> I am detail < /div>);
 					break;
 			}
 			return (
